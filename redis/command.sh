fc925fa078775b86df816e0b42f6ec6f6fcc99d7
root <root@bi-mybeectrl.freeipa.bee.skymobile.local>
2025-05-21 06:06:31 +0600

    Ansible

--- redis/command.sh
+++ redis/command.sh
@@ -2,4 +2,4 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm upgrade --install redis bitnami/redis \
   --namespace game-platform-kg \
   --create-namespace \
-  -f values-longhorn.yaml
+  -f values.yml
